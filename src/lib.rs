6598e8a4cca9636299b19dc992fe78e098c53548
Async <asyncvoid@gmail.com>
2023-06-26 00:28:06 +0100

    documentation and license

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,65 @@ use quote::{quote};
 use syn::punctuated::Punctuated;
 use syn::spanned::Spanned;
 
-/// Example of [function-like procedural macro][1].
+//! # envime
+//!
+//! This crate provides a procedural macro that can retrieve an environment variable at compile time or runtime.
+//!
+//! Specify an environment variable at compile time and the runtime variable will never be evaluated.
+//! Don't specify the environment variable at compile-time and it will be evaluated at runtime.
+//!
+//! ## Examples
+//!
+//! ```
+//! use std::env;
+//! use envtime::*;
+//!
+//! // You can use unwrap_or_else on the envtime macro
+//! let var = envtime!("TEST_NON_ENV").unwrap_or_else(|| String::from("hello"));
+//! assert_eq!(var, String::from("012"));
+//! // This resolves to "hello" assuming it is not defined at compile-time or runtime
+//!
+//! // Lets set a runtime variable to "123"
+//! env::set_var("TEST_RUN_ENV", "123");
+//! let var = envtime!("TEST_RUN_ENV");
+//! assert_eq!(var, Some(String::from("123")));
+//! // And you can see it gets the value
+//!
+//! // Assume we have a compile-time variable set to "456"
+//! env::set_var("TEST_COMP_ENV", "123"); // We set the runtime variable to "123"
+//! let var = envtime!("TEST_COMP_ENV");
+//! assert_eq!(var, Some(String::from("456")));
+//! // And the runtime variable is ignored, as the macro resolves to 'String::from("456")' at compile time
+//!
+//! // Assume we don't have the runtime variable set at first, you can see the default value being used
+//! assert_eq!(envtime_def!("TEST_STR_RUN_ENV", "test"), "test");
+//! env::set_var("TEST_STR_RUN_ENV", "not");
+//! assert_eq!(envtime_def!("TEST_STR_RUN_ENV", "test"), "not");
+//! // And once it is set it resolves to our runtime value
+//!
+//! // Assume we have "TEST_BOOL_COMP_ENV" at compile-time to "true"
+//! env::set_var("TEST_BOOL_COMP_ENV", "false"); // Sets the runtime-variable, which is ignored
+//! let enable_test = envtime_def!("TEST_BOOL_COMP_ENV", false);  // Resolves to the literal "true"
+//! assert_eq!(enable_test, true);
+//! // With the default value being false, and the runtime value being false, it still evaluates to true
+//!
+//! // Example with u8
+//! assert_eq!(envtime_def!("TEST_U8_RUN_ENV", 77u8), 77u8);
+//! env::set_var("TEST_U8_RUN_ENV", "53");
+//! assert_eq!(envtime_def!("TEST_U8_RUN_ENV", 77u8), 53u8);
+//! ```
+
+/// Gets a environment variable as a String either at compile or runtime
+/// # Example
+/// ```
+/// use envtime::*;
 ///
-/// [1]: https://doc.rust-lang.org/reference/procedural-macros.html#function-like-procedural-macros
+/// // Assuming the variable isn't set
+/// let var = envtime!("DOMAIN");
+/// assert_eq!(var, None);
+/// let var = var.unwrap_or_else(|| String::from("example.com"));
+/// assert_eq!(var, String::from("example.com"));
+/// ```
 #[proc_macro]
 pub fn envtime(input: TokenStream) -> TokenStream {
     let lit_str = parse_macro_input!(input as LitStr);
@@ -24,9 +80,15 @@ pub fn envtime(input: TokenStream) -> TokenStream {
     }.into()
 }
 
-/// Example of [function-like procedural macro][1].
+/// Gets a environment variable as the type specified by the default value, either at compile or runtime
+/// # Example
+/// ```
+/// use envtime::*;
 ///
-/// [1]: https://doc.rust-lang.org/reference/procedural-macros.html#function-like-procedural-macros
+/// // Assuming we set the value of "PORT" to 5678 at compile or runtime
+/// let var = envtime_def!("PORT", 1234u16);
+/// assert_eq!(var, 5678u16);
+/// ```
 #[proc_macro]
 pub fn envtime_def(input: TokenStream) -> TokenStream {
     let input : Punctuated<Lit,Token![,]> = parse_macro_input!(input with Punctuated<Lit,Token![,]>::parse_terminated);
